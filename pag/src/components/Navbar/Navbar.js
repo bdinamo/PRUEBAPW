8766bed4d33ff124163488f351eb79379439b3a7
gaston <gnghigo@mail.austral.edu.ar>
2021-10-23 17:03:20 -0300

    gaston

--- pag/src/components/Navbar/Navbar.js
+++ pag/src/components/Navbar/Navbar.js
@@ -5,10 +5,12 @@ const Navbar = () => {
 
 
   return (<div className="navbar">
+    <Link to={'/'}><h4 className="navbar-element">inicio</h4></Link>
   <Link to={'/profile'}><h4 className="navbar-element">Ingresá</h4></Link>
   <Link to={'/register'}><h4 className="navbar-element">Registrate</h4></Link>
   <Link to={'/courses'}><h4 className="navbar-element">Mis Compras</h4></Link>
   <Link to={'/courses'}><h4 className="navbar-element">Productos</h4></Link>
+
 </div>)
 }
 export default Navbar;
