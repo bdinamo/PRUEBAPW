470393f8074fa52ce82d6c2f04a430097c8a8211
BDINAMO <briandinamo@hotmail.com>
2021-11-15 23:28:14 -0300

    tipo de dato en price

--- backpw/api/models.py
+++ backpw/api/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Products(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
-    price = models.CharField(max_length=10)
+    price = models.FloatField(default=0)
     tipoproducto = models.CharField(max_length=15)
+
 
